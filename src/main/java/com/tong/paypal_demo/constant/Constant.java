e24f7bf65c8e30f5f22fcedce62596d7b1b2e94f
tong <oncwnuOh0fwZryEHNHFQW2PNJYQI@git.weixin.qq.com>
2022-09-28 18:17:48 +0800

    init

---
+++ src/main/java/com/tong/paypal_demo/constant/Constant.java
@@ -0,0 +1,12 @@
+package com.tong.paypal_demo.constant;
+
+/**
+ * Created by tong on 2022/09/28
+ */
+public class Constant {
+
+    public static final String BASE_URL = "https://api-m.sandbox.paypal.com";
+    public static final String client_id = "";
+    public static final String client_secret = "";
+
+}
