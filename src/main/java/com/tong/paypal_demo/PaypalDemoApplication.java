0ad6528370850edf87ab6d4973057a3b836c19dd
tong <oncwnuOh0fwZryEHNHFQW2PNJYQI@git.weixin.qq.com>
2022-09-28 14:17:20 +0800

    Initial commit

---
+++ src/main/java/com/tong/paypal_demo/PaypalDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tong.paypal_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaypalDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaypalDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tong/paypal_demo/PaypalDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tong.paypal_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaypalDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
